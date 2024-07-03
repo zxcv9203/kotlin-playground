d2e32a5f187bd11a8f2adc728970069877f37de7
yongckim <zxcv9203@naver.com>
2024-07-03 20:34:27 +0900

    :memo: 코틀린 자동 리소스 해제 설명 주석 추가

--- java-to-kotlin/src/main/kotlin/org/example/section2/TryWithResources.kt
+++ java-to-kotlin/src/main/kotlin/org/example/section2/TryWithResources.kt
@@ -3,6 +3,11 @@ package org.example.section2
 import java.io.BufferedReader
 import java.io.FileReader
 
+/**
+ * 코틀린에서는 try-with-resources 구문이 없는대신 언어적 특징을 활용해 close를 호출해줍니다.
+ *
+ * use 확장함수를 사용하면 BufferedReader를 사용한 후 자동으로 close를 호출해줍니다.
+ */
 fun readFile(path: String) = BufferedReader(FileReader(path)).use { reader ->
     println(reader.readLine())
 }
