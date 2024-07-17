1ecaa6852e0d4c3fad5076f3a3ef7d08f939257c
yongckim <zxcv9203@naver.com>
2024-07-17 20:37:12 +0900

    :white_check_mark: 책 관련 서비스 테스트 추가

--- java-to-kotlin-application/src/main/java/com/group/libraryapp/domain/user/loanhistory/UserLoanHistory.java
+++ java-to-kotlin-application/src/main/java/com/group/libraryapp/domain/user/loanhistory/UserLoanHistory.java
@@ -1,6 +1,7 @@
 package com.group.libraryapp.domain.user.loanhistory;
 
 import com.group.libraryapp.domain.user.User;
+import org.jetbrains.annotations.NotNull;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
@@ -12,33 +13,43 @@ import static javax.persistence.GenerationType.IDENTITY;
 @Entity
 public class UserLoanHistory {
 
-  @Id
-  @GeneratedValue(strategy = IDENTITY)
-  private Long id;
+    @Id
+    @GeneratedValue(strategy = IDENTITY)
+    private Long id;
 
-  @ManyToOne
-  private User user;
+    @ManyToOne
+    private User user;
 
-  private String bookName;
+    private String bookName;
 
-  private boolean isReturn;
+    private boolean isReturn;
 
-  public UserLoanHistory() {
+    public UserLoanHistory() {
 
-  }
+    }
 
-  public UserLoanHistory(User user, String bookName, boolean isReturn) {
-    this.user = user;
-    this.bookName = bookName;
-    this.isReturn = isReturn;
-  }
+    public UserLoanHistory(User user, String bookName, boolean isReturn) {
+        this.user = user;
+        this.bookName = bookName;
+        this.isReturn = isReturn;
+    }
 
-  public String getBookName() {
-    return this.bookName;
-  }
+    @NotNull
+    public String getBookName() {
+        return this.bookName;
+    }
 
-  public void doReturn() {
-    this.isReturn = true;
-  }
+    public void doReturn() {
+        this.isReturn = true;
+    }
+
+    @NotNull
+    public User getUser() {
+        return user;
+    }
+
+    public boolean isReturn() {
+        return this.isReturn;
+    }
 
 }
---
+++ java-to-kotlin-application/src/test/kotlin/com/group/libraryapp/service/book/BookServiceTest.kt
@@ -0,0 +1,103 @@
+package com.group.libraryapp.service.book
+
+import com.group.libraryapp.domain.book.Book
+import com.group.libraryapp.domain.book.BookRepository
+import com.group.libraryapp.domain.user.User
+import com.group.libraryapp.domain.user.UserRepository
+import com.group.libraryapp.domain.user.loanhistory.UserLoanHistory
+import com.group.libraryapp.domain.user.loanhistory.UserLoanHistoryRepository
+import com.group.libraryapp.dto.book.request.BookLoanRequest
+import com.group.libraryapp.dto.book.request.BookRequest
+import com.group.libraryapp.dto.book.request.BookReturnRequest
+import org.assertj.core.api.Assertions.assertThat
+import org.assertj.core.api.Assertions.assertThatThrownBy
+import org.junit.jupiter.api.AfterEach
+import org.junit.jupiter.api.DisplayName
+import org.junit.jupiter.api.Nested
+import org.junit.jupiter.api.Test
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class BookServiceTest @Autowired constructor(
+    private val bookRepository: BookRepository,
+    private val bookService: BookService,
+    private val userRepository: UserRepository,
+    private val userLoanHistoryRepository: UserLoanHistoryRepository,
+) {
+
+    @AfterEach
+    fun tearDown() {
+        bookRepository.deleteAll()
+        userRepository.deleteAll()
+    }
+
+    @Nested
+    inner class SaveBook {
+
+        @Test
+        @DisplayName("[성공] 책 생성에 성공한다.")
+        fun successTest() {
+            val request = BookRequest("테스트")
+
+            bookService.saveBook(request)
+
+            val want = bookRepository.findAll()
+            assertThat(want).hasSize(1)
+            assertThat(want[0].name).isEqualTo("테스트")
+        }
+    }
+
+    @Nested
+    inner class LoanBook {
+
+
+        @Test
+        @DisplayName("[성공] 책 대출에 성공한다.")
+        fun successTest() {
+            val savedBook = bookRepository.save(Book("테스트"))
+            val savedUser = userRepository.save(User("아무개", null))
+            val request = BookLoanRequest("아무개", "테스트")
+
+            bookService.loanBook(request)
+
+            val want = userLoanHistoryRepository.findAll()
+            assertThat(want).hasSize(1)
+            assertThat(want[0].bookName).isEqualTo(savedBook.name)
+            assertThat(want[0].user.id).isEqualTo(savedUser.id)
+            assertThat(want[0].isReturn).isFalse()
+        }
+
+        @Test
+        @DisplayName("[실패] 책이 대출되어 있다면, 신규 대출이 실패합니다.")
+        fun alreadyLoanTest() {
+            val savedBook = bookRepository.save(Book("테스트"))
+            val savedUser = userRepository.save(User("아무개", null))
+            userLoanHistoryRepository.save(UserLoanHistory(savedUser, savedBook.name, false))
+            val request = BookLoanRequest("아무개", "테스트")
+
+            assertThatThrownBy { bookService.loanBook(request) }
+                .isInstanceOf(IllegalArgumentException::class.java)
+                .hasMessage("진작 대출되어 있는 책입니다")
+        }
+
+    }
+
+    @Nested
+    inner class ReturnBook {
+        @Test
+        @DisplayName("[성공] 책 반납이 성공한다.")
+        fun returnBookTest() {
+            val savedBook = bookRepository.save(Book("테스트"))
+            val savedUser = userRepository.save(User("아무개", null))
+            userLoanHistoryRepository.save(UserLoanHistory(savedUser, savedBook.name, false))
+            val request = BookReturnRequest("아무개", "테스트")
+
+            bookService.returnBook(request)
+
+            val want = userLoanHistoryRepository.findAll()
+            assertThat(want).hasSize(1)
+            assertThat(want[0].isReturn).isTrue()
+        }
+    }
+}
