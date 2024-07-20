abc1b0d2692a6455b3d9feb9c9348ee5e0854168
yongckim <zxcv9203@naver.com>
2024-07-20 19:48:41 +0900

    :recycle: dto를 kotlin으로 마이그레이션

--- java-to-kotlin-application/src/main/kotlin/com/group/libraryapp/service/user/UserService.kt
+++ java-to-kotlin-application/src/main/kotlin/com/group/libraryapp/service/user/UserService.kt
@@ -7,7 +7,6 @@ import com.group.libraryapp.dto.user.request.UserUpdateRequest
 import com.group.libraryapp.dto.user.response.UserResponse
 import com.group.libraryapp.util.fail
 import com.group.libraryapp.util.findByIdOrThrow
-import org.springframework.data.repository.findByIdOrNull
 import org.springframework.stereotype.Service
 import org.springframework.transaction.annotation.Transactional
 
@@ -28,7 +27,7 @@ class UserService(
 
     @Transactional(readOnly = true)
     fun getUsers(): List<UserResponse> = userRepository.findAll()
-        .map(::UserResponse)
+        .map(UserResponse::of)
 
     @Transactional
     fun updateUserName(request: UserUpdateRequest) {
