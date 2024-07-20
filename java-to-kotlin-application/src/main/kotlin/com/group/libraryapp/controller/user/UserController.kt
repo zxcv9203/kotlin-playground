10df9a9f438fc8182e30f1aa74c23459cb154038
yongckim <zxcv9203@naver.com>
2024-07-20 19:57:30 +0900

    :recycle: 컨트롤러와 메인함수를 kotlin으로 마이그레이션

---
+++ java-to-kotlin-application/src/main/kotlin/com/group/libraryapp/controller/user/UserController.kt
@@ -0,0 +1,41 @@
+package com.group.libraryapp.controller.user
+
+import com.group.libraryapp.dto.user.request.UserCreateRequest
+import com.group.libraryapp.dto.user.request.UserUpdateRequest
+import com.group.libraryapp.dto.user.response.UserResponse
+import com.group.libraryapp.service.user.UserService
+import org.springframework.web.bind.annotation.DeleteMapping
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.PutMapping
+import org.springframework.web.bind.annotation.RequestBody
+import org.springframework.web.bind.annotation.RequestParam
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+class UserController(
+    private val userService: UserService
+) {
+
+    @PostMapping("/user")
+    fun saveUser(@RequestBody request: UserCreateRequest) {
+        userService.saveUser(request)
+    }
+
+    @GetMapping("/user")
+    fun getUsers(): List<UserResponse> {
+        return userService.getUsers()
+    }
+
+    @PutMapping("/user")
+    fun updateUserName(@RequestBody request: UserUpdateRequest) {
+        userService.updateUserName(request)
+    }
+
+
+    // ! 주의 : RequestParam의 타입을 Nullable 타입으로 받는 경우 required를 자동으로 false로 처리한다.
+    @DeleteMapping("/user")
+    fun deleteUser(@RequestParam name: String) {
+        userService.deleteUser(name)
+    }
+}
