53dcd750b2bc5339e768adea03f6edde9c23e2e3
yongckim <zxcv9203@naver.com>
2024-07-20 19:41:21 +0900

    :recycle: Optional을 제거하고 코틀린의 null 가능 타입을 사용하도록 변경

--- java-to-kotlin-application/src/main/kotlin/com/group/libraryapp/service/book/BookService.kt
+++ java-to-kotlin-application/src/main/kotlin/com/group/libraryapp/service/book/BookService.kt
@@ -7,6 +7,7 @@ import com.group.libraryapp.domain.user.loanhistory.UserLoanHistoryRepository
 import com.group.libraryapp.dto.book.request.BookLoanRequest
 import com.group.libraryapp.dto.book.request.BookRequest
 import com.group.libraryapp.dto.book.request.BookReturnRequest
+import com.group.libraryapp.util.fail
 import org.springframework.stereotype.Service
 import org.springframework.transaction.annotation.Transactional
 
@@ -27,21 +28,19 @@ class BookService(
 
     @Transactional
     fun loanBook(request: BookLoanRequest) {
-        val book = bookRepository.findByName(request.bookName)
-            .orElseThrow(::IllegalArgumentException)
+        val book = bookRepository.findByName(request.bookName) ?: fail()
 
         if (userLoanHistoryRepository.findByBookNameAndIsReturn(request.bookName, false) != null) {
             throw IllegalArgumentException("진작 대출되어 있는 책입니다")
         }
-        val user = userRepository.findByName(request.userName)
-            .orElseThrow(::IllegalArgumentException)
+        val user = userRepository.findByName(request.userName) ?: fail()
+
         user.loanBook(book)
     }
 
     @Transactional
     fun returnBook(request: BookReturnRequest) {
-        val user = userRepository.findByName(request.userName)
-            .orElseThrow(::IllegalArgumentException)
+        val user = userRepository.findByName(request.userName) ?: fail()
 
         user.returnBook(request.bookName)
     }
