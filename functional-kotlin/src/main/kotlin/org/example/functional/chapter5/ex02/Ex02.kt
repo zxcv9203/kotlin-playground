9804c08e93dc69fc36f66540cedfe6bf722b1c2b
yongckim <zxcv9203@naver.com>
2025-03-03 21:51:54 +0900

    코틀린 아카데미 - 함수형 프로그레밍 : 확장함수 참조 예시

---
+++ functional-kotlin/src/main/kotlin/org/example/functional/chapter5/ex02/Ex02.kt
@@ -0,0 +1,23 @@
+package org.example.functional.chapter5.ex02
+
+import org.example.functional.chapter5.Number
+
+data class Number(val num: Int)
+
+fun Number.toFloat(): Float = num.toFloat()
+fun Number.times(n: Int): Number = Number(num * n)
+
+fun main() {
+    val numberObject = Number(10)
+
+    // 확장 함수 참조
+    val float: Number.() -> Float = Number::toFloat
+
+    // toFloat에는 매개변수가 없지만,
+    // 함수 타입에 Number 타입의 리시버가 필요
+    println(float(numberObject))
+
+    val multiply: Number.(Int) -> Number = Number::times
+    // times에는 매개변수가 Int 타입 하나뿐이지만, 그 함수 타입에는 Number 타입의 리시버가 필요합니다.
+    println(multiply(numberObject, 4))
+}
