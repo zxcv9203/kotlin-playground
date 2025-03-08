17b7fcbb81384f5ff7f16d8dd2e08e2cf026a5de
yongckim <zxcv9203@naver.com>
2025-03-09 02:59:23 +0900

    코틀린 아카데미 - 함수형 프로그래밍 : 함수를 사용하지 않은 버전 예시

---
+++ functional-kotlin/src/main/kotlin/org/example/functional/chapter7/Main2.kt
@@ -0,0 +1,11 @@
+package org.example.functional.chapter7
+
+
+fun main() {
+    val students = listOf(Student("Alice", 70), Student("Bob", 80))
+    var points = 0
+    for (student in students) {
+        points += student.points
+    }
+    println(points)
+}
