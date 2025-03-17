07e43b4b03c51e25da10d0f59f23259cd24983d8
yongckim <zxcv9203@naver.com>
2025-03-17 22:10:39 +0900

    코틀린 아카데미 - 함수형 프로그래밍 : sorted 예시

---
+++ functional-kotlin/src/main/kotlin/org/example/functional/chapter8/ex18/Sorted.kt
@@ -0,0 +1,8 @@
+package org.example.functional.chapter8.ex18
+
+fun main() {
+    println(listOf(4, 1, 3, 2).sorted())
+    println(listOf('b', 'A', 'a', ' ', 'B').sorted())
+    println(listOf("Bab", "AAZ", "Bza", "A").sorted())
+    println(listOf(true, false, true).sorted())
+}
