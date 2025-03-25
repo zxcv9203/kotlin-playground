7772530ba1e61208d2ba07c9f0024710961bf5de
yongckim <zxcv9203@naver.com>
2025-03-25 21:34:06 +0900

    코틀린 아카데미 - 함수형 프로그래밍 : 시퀀스의 지연 연산 예시

---
+++ functional-kotlin/src/main/kotlin/org/example/functional/chapter9/ex02/Sequence.kt
@@ -0,0 +1,8 @@
+package org.example.functional.chapter9.ex02
+
+fun main() {
+    sequenceOf(1, 2, 3)
+        .filter { print("F$it, "); it % 2 == 1 }
+        .map { it * 2 }
+        .forEach { print("M$it, ") }
+}
