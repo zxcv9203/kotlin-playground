87c0e6a6231bc001bd6e5b1e5e928d2d771e5355
yongckim <zxcv9203@naver.com>
2025-06-23 22:09:16 +0900

    ParameterizedTest

--- practice-testing/src/test/kotlin/org/example/practicetesting/spring/domain/product/ProductTypeTest.kt
+++ practice-testing/src/test/kotlin/org/example/practicetesting/spring/domain/product/ProductTypeTest.kt
@@ -2,6 +2,8 @@ package org.example.practicetesting.spring.domain.product
 
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.DisplayName
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.CsvSource
 import kotlin.test.Test
 
 class ProductTypeTest {
@@ -13,5 +15,17 @@ class ProductTypeTest {
         val result = ProductType.containsStockType(type)
 
         assertThat(result).isTrue
+    }
+
+    @ParameterizedTest
+    @CsvSource(value = ["HANDMADE,false", "BOTTLE,true", "BAKERY,true"], delimiter = ',')
+    @DisplayName("상품 타입이 재고 관련 타입인지 여부를 확인합니다.")
+    fun containsStockType2(
+        productType: ProductType,
+        expected: Boolean,
+    ) {
+        val result = ProductType.containsStockType(productType)
+
+        assertThat(result).isEqualTo(expected)
     }
 }
