fc698ab58c1f2d397deac135d571822185b1c65c
yongckim <zxcv9203@naver.com>
2025-06-27 22:51:06 +0900

    spring tdd 강의

---
+++ tdd/src/main/kotlin/org/example/tdd/TddApplication.kt
@@ -0,0 +1,11 @@
+package org.example.tdd
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class TddApplication
+
+fun main(args: Array<String>) {
+    runApplication<TddApplication>(*args)
+}
---
+++ tdd/src/test/kotlin/org/example/tdd/TddApplicationTests.kt
@@ -0,0 +1,11 @@
+package org.example.tdd
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class TddApplicationTests {
+    @Test
+    fun contextLoads() {
+    }
+}
