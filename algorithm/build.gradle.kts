89aa8f673cb81a0257c885afcfebd6100872c563
yongckim <zxcv9203@naver.com>
2025-09-22 22:44:47 +0900

    알고리즘 : 문자리스트를 문자열로 변환하기

---
+++ algorithm/build.gradle.kts
@@ -0,0 +1,21 @@
+plugins {
+    kotlin("jvm") version "2.2.10"
+}
+
+group = "com.localtalk"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    testImplementation(kotlin("test"))
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
+kotlin {
+    jvmToolchain(21)
+}
---
+++ algorithm/src/main/kotlin/programmers/level0/lesson181941/Main.kt
@@ -0,0 +1,8 @@
+package programmers.level0.lesson181941
+
+fun solution(arr: Array<String>): String = arr.joinToString("")
+
+fun main() {
+    val arr = arrayOf("1", "2", "3")
+    println(solution(arr))
+}
