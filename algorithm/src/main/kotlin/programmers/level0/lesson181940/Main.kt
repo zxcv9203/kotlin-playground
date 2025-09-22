af86c99f1ae9f353eaface959ce1511f7dc2a00c
yongckim <zxcv9203@naver.com>
2025-09-22 23:34:12 +0900

    알고리즘 : 더 크게 합치기

--- algorithm/src/main/kotlin/programmers/level0/lesson181940/Main.kt
+++ algorithm/src/main/kotlin/programmers/level0/lesson181940/Main.kt
@@ -1,12 +1,16 @@
 package programmers.level0.lesson181940
 
 fun solution(
-    my_string: String,
-    k: Int,
-): String = my_string.repeat(k)
+    a: Int,
+    b: Int,
+): Int =
+    maxOf(
+        "$a$b".toInt(),
+        "$b$a".toInt(),
+    )
 
 fun main() {
     val a = "string"
     val k = 3
-    println(solution(a, k))
+    println(solution(11, 3))
 }
