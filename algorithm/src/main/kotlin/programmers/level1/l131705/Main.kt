1f6012b5afbbd64732ff511acb6cec355a94bc37
yongckim <zxcv9203@naver.com>
2025-09-25 21:01:11 +0900

    레벨 0 -> 레벨 1로 이동

--- algorithm/src/main/kotlin/programmers/level1/l131705/Main.kt
+++ algorithm/src/main/kotlin/programmers/level1/l131705/Main.kt
@@ -1,10 +1,10 @@
-package programmers.level0.l131705
+package programmers.level1.l131705
 
-fun solution(number: IntArray): Int {
-    return number.indices.toList()
+fun solution(number: IntArray): Int =
+    number.indices
+        .toList()
         .combinations(3)
         .count { (i, j, k) -> number[i] + number[j] + number[k] == 0 }
-}
 
 private fun <T> List<T>.combinations(r: Int): List<List<T>> {
     if (r == 0) return listOf(emptyList())
@@ -12,7 +12,10 @@ private fun <T> List<T>.combinations(r: Int): List<List<T>> {
 
     val result = mutableListOf<List<T>>()
 
-    fun backtrack(start: Int, current: MutableList<T>) {
+    fun backtrack(
+        start: Int,
+        current: MutableList<T>,
+    ) {
         if (current.size == r) {
             result.add(current.toList())
             return
---
+++ algorithm/src/main/kotlin/programmers/level1/l147355/Main.kt
@@ -0,0 +1,12 @@
+package programmers.level1.l147355
+
+fun solution(
+    t: String,
+    p: String,
+): Int = t.windowed(p.length).count { it <= p }
+
+fun main() {
+    val t = "100"
+    val p = "23"
+    println(solution(t, p))
+}
