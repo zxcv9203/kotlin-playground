fb2b6fdc4b5169a4f2f913522dae93d81e7fc598
yongckim <zxcv9203@naver.com>
2025-09-25 20:58:27 +0900

    알고리즘 : 삼총사

---
+++ algorithm/src/main/kotlin/programmers/level0/l131705/Main.kt
@@ -0,0 +1,30 @@
+package programmers.level0.l131705
+
+fun solution(number: IntArray): Int {
+    return number.indices.toList()
+        .combinations(3)
+        .count { (i, j, k) -> number[i] + number[j] + number[k] == 0 }
+}
+
+private fun <T> List<T>.combinations(r: Int): List<List<T>> {
+    if (r == 0) return listOf(emptyList())
+    if (r > size) return emptyList()
+
+    val result = mutableListOf<List<T>>()
+
+    fun backtrack(start: Int, current: MutableList<T>) {
+        if (current.size == r) {
+            result.add(current.toList())
+            return
+        }
+
+        for (i in start until size) {
+            current.add(this[i])
+            backtrack(i + 1, current)
+            current.removeAt(current.lastIndex)
+        }
+    }
+
+    backtrack(0, mutableListOf())
+    return result
+}
