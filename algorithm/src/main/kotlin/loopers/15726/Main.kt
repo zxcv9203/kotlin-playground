6238606468ff46af4c8254e98ebd6f167a5432c3
yongckim <zxcv9203@naver.com>
2026-03-09 20:29:26 +0900

    알고리즘 풀이

---
+++ algorithm/src/main/kotlin/loopers/15726/Main.kt
@@ -0,0 +1,13 @@
+package loopers.`15726`
+
+
+fun main() {
+    val list = readln().split(" ").map { it.toLong() }
+
+    val a = list[0] * list[2] / list[1]
+    val b = list[0] * list[1] / list[2]
+
+    val result = maxOf(a, b)
+
+    println(result)
+}
