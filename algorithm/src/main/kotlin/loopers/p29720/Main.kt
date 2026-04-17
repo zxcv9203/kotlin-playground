aba073711a0d7b3422c251975e40f36609082b57
yongckim <zxcv9203@naver.com>
2026-04-17 21:54:37 +0900

    알고리즘 풀이

--- algorithm/src/main/kotlin/loopers/p29720/Main.kt
+++ algorithm/src/main/kotlin/loopers/p29720/Main.kt
@@ -3,9 +3,9 @@ package loopers.p29720
 import kotlin.math.max
 
 fun main() {
-    val (x, d) = readln().split(" ").map { it.toInt() }
+    val (n, m, k) = readln().split(" ").map { it.toInt() }
 
-    val min = max(0, 1000 - x * d)
-    val max = 1000 - x * (d - 1) - 1
+    val min = max(0, n - m * k)
+    val max = n - m * (k - 1) - 1
     println("$min $max")
 }
